a865f84990b0229f5dfecd3258226207ba75ef39
unknown <jacejoji@gmail.com>
2025-11-03 17:21:27 +0530

    Update

--- src/com/example/Customer1.java
+++ src/com/example/Customer1.java
@@ -1,11 +1,23 @@
 package com.example;
-
 public class Customer1 extends SBIBank{
-
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		Customer1 c=new Customer1();
 		System.out.println(c.interestRate(1000));
+		Animal1 a=new Animal1() {
+			@Override
+			public void eat() {
+				// TODO Auto-generated method stub
+			}
+			@Override
+			public void sleep() {
+				// TODO Auto-generated method stub
+			}
+			@Override
+			public void run() {
+				// TODO Auto-generated method stub
+			}}
+		;
 	}
 
 }
